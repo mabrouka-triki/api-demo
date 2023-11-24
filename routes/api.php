74f2d026fd7f6c3954448d32d4d23ae23f5de101
mtrik <trikimabrouka09@gmail.com>
2023-11-24 10:13:09 +0100

    add fake champs

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProduitsController;
+use App\Http\Controllers\CommandesController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/produits', [ProduitsController::class, "liste"]);
+Route::get('/produits/{id}', [ProduitsController::class, "detail"]);
+Route::post('/ajoutcommande', [CommandesController::class, "ajoutercommande"]);
